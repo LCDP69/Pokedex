f6b2d6d4a5292655f618ca8811413a6d88b32ca5
LCDP69 <nicolas38.gros@gmail.com>
2023-12-04 11:15:43 +0100

    quête 3

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -4,10 +4,28 @@ import React from 'react';
 const PokemonCard = () => {
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemonList.map((pokemon) => (
+                <>
+                    {pokemon.imgSrc ? (<> <img src={pokemon.imgSrc} alt={pokemon.name} />
+                        <figcaption>{pokemon.name}</figcaption></>) : <figcaption><p>???</p></figcaption>}
+                </>
+            ))}
+
         </figure>
     );
+
 };
+
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 export default PokemonCard;
